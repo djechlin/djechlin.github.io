184103a421bd695e7926bc9bd8ae3a803e21bfaf
Dan Echlin <djechlin@gmail.com>
2024-11-14 21:33:08 -0500

    base is forward slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://djechlin.github.io',
+  base: '/',
   integrations: [react(), tailwind({
     applyBaseStyles: false
   })]
