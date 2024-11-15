8714805911a4f930297fa29667aa86614c66807f
Dan Echlin <djechlin@gmail.com>
2024-11-14 21:27:21 -0500

    back to djechlin.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://danielechlin.xyz',
+  site: 'https://djechlin.github.io',
   integrations: [react(), tailwind({
     applyBaseStyles: false
   })]
