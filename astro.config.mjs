c7c535e0dd10b9a249fed92326363149a1f5b2fa
Dan Echlin <djechlin@gmail.com>
2024-12-24 16:01:33 -0500

    support mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
+import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://danielechlin.xyz',
-  integrations: [react(), tailwind({
+  integrations: [react(), mdx(), tailwind({
     applyBaseStyles: false
   })],
   vite: {
